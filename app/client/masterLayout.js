c57a9f845c4d4815a4daea1dbd50a3ff5d6ef7fe
Christian Kiely <ckiely91@gmail.com>
2016-01-28 19:27:24 +1100

    Add privacy policy

--- app/client/masterLayout.js
+++ app/client/masterLayout.js
@@ -98,3 +98,8 @@ Template.notificationInfoModal.onRendered(function() {
 		observeChanges: true
 	});
 });
+
+Template.privacy.onRendered(function() {
+	//privacy policy thing
+	(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
+});
