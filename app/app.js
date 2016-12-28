e910ed03c24c4da8cb428be8365ac38301eeca84
Christian Kiely <ckiely91@gmail.com>
2016-12-28 17:05:42 +1100

    Update last updated date

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import lodash from 'lodash';
 
 // Global version vars
 appVersion = require('./package.json').version;
-appLastUpdated = "4 Dec 2016";
+appLastUpdated = "28 Dec 2016";
 
 // Replace global underscore with lodash
 _ = lodash;
