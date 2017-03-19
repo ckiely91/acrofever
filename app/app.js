c2530b569657171d92754fa316db6dca970c3121
Christian Kiely <ckiely91@gmail.com>
2017-03-19 11:31:26 +1100

    Update package version

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import lodash from 'lodash';
 
 // Global version vars
 appVersion = require('./package.json').version;
-appLastUpdated = "20 Feb 2016";
+appLastUpdated = "19 Mar 2016";
 
 // Replace global underscore with lodash
 _ = lodash;
